6a79e711947acadadfb1908762d915f1d446f777
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2026-03-21 19:40:23 +0530

    Implemented simple profiler with anchor and stack

--- nob_profiler.h
+++ nob_profiler.h
@@ -7,6 +7,9 @@
 */
 
 #include <stdint.h>
+#ifndef NOB_PROFILER_NO_STDLIB
+#include <stdlib.h>
+#endif // NOB_PROFILER_NO_STDLIB
 
 typedef uint32_t u32;
 typedef uint64_t u64;
@@ -17,6 +20,52 @@ u64 nob_get_os_timer_freq(void);
 u64 nob_read_cpu_timer(void);
 f64 nob_guess_cpu_timer_freq(u32 wait_time_in_millis);
 f64 nob_measure_time_in_millis_from_elapsed(u64 elapsed, f64 freq);
+
+#ifndef NOB_PROFILER_NO_STDLIB
+#define NOB_ANCHORS_RESERVE_SIZE (1 << 12)
+#define NOB_BLOCKS_RESERVE_SIZE  (1 << 16)
+
+typedef struct {
+    size_t anchor_idx;
+    const char *label;
+    u64 total_elapsed_including_children;
+    u64 total_elapsed_excluding_children;
+    size_t hit_count;
+    u64 first_start;
+} Nob_Profile_Anchor;
+
+typedef struct {
+    Nob_Profile_Anchor *items;
+    size_t count;
+    size_t capacity;
+} Nob_Profile_Anchors;
+
+typedef struct {
+    size_t anchor_idx;
+    size_t parent_idx;
+    u64 start;
+    u64 old_total_elapsed_including_children;
+} Nob_Profile_Block;
+
+typedef struct {
+    Nob_Profile_Block *items;
+    size_t count;
+    size_t capacity;
+} Nob_Profile_Blocks;
+
+typedef struct {
+    Nob_Profile_Anchors anchors;
+    Nob_Profile_Blocks blocks;
+    u64 start;
+    f64 cpu_freq;
+} Nob_Profiler;
+
+void nob_reset_profiler(Nob_Profiler *profiler);
+void nob_start_profile_at_anchor(Nob_Profiler *profiler, const char *label, size_t anchor_idx);
+#define nob_start_profile(profiler, label) nob_start_profile_at_anchor(profiler, label, __COUNTER__ + 1);
+void nob_end_profile(Nob_Profiler *profiler);
+void nob_log_profiler(Nob_Profiler profiler);
+#endif // NOB_PROFILER_NO_STDLIB
 
 #ifdef NOB_PROFILER_IMPLEMENTATION
 
@@ -91,6 +140,90 @@ f64 nob_measure_time_in_millis_from_elapsed(u64 elapsed, f64 freq) {
     return ((f64) elapsed / freq) * 1000;
 }
 
+#ifndef NOB_PROFILER_NO_STDLIB
+
+void nob_reset_profiler(Nob_Profiler *profiler) {
+    profiler->anchors.count = 0;
+    profiler->blocks.count = 0;
+    nob_da_reserve(&profiler->anchors, NOB_ANCHORS_RESERVE_SIZE);
+    nob_da_reserve(&profiler->blocks, NOB_BLOCKS_RESERVE_SIZE);
+    nob_da_append(&profiler->anchors, ((Nob_Profile_Anchor) {0}));
+    profiler->cpu_freq = nob_guess_cpu_timer_freq(100);
+    profiler->start = nob_read_cpu_timer();
+}
+
+void nob_start_profile_at_anchor(Nob_Profiler *profiler, const char *label, size_t anchor_idx) {
+    while (anchor_idx >= profiler->anchors.count) {
+        nob_da_append(&profiler->anchors, ((Nob_Profile_Anchor) {0}));
+    }
+    Nob_Profile_Anchor *anchor = &profiler->anchors.items[anchor_idx];
+    anchor->anchor_idx = anchor_idx;
+    anchor->label = label;
+    Nob_Profile_Block block = {0};
+    block.anchor_idx = anchor_idx;
+    if (profiler->blocks.count > 0) {
+        block.parent_idx = nob_da_last(&profiler->blocks).anchor_idx;
+    }
+    block.start = nob_read_cpu_timer();
+    if (anchor->first_start == 0) {
+        anchor->first_start = block.start;
+    }
+    block.old_total_elapsed_including_children = anchor->total_elapsed_including_children;
+    nob_da_append(&profiler->blocks, block);
+}
+
+void nob_end_profile(Nob_Profiler *profiler) {
+    Nob_Profile_Block block = nob_da_pop(&profiler->blocks);
+    u64 elapsed = nob_read_cpu_timer() - block.start;
+    Nob_Profile_Anchor *anchor = &profiler->anchors.items[block.anchor_idx];
+    anchor->total_elapsed_excluding_children += elapsed;
+    anchor->total_elapsed_including_children = block.old_total_elapsed_including_children + elapsed;
+    anchor->hit_count++;
+    if (profiler->blocks.count > 0) {
+        Nob_Profile_Anchor *parent = &profiler->anchors.items[block.parent_idx];
+        parent->total_elapsed_excluding_children -= elapsed;
+    }
+}
+
+int nob__cmp_by_first_start(const void *a, const void *b) {
+    const Nob_Profile_Anchor *anchor_a = (const Nob_Profile_Anchor *)a;
+    const Nob_Profile_Anchor *anchor_b = (const Nob_Profile_Anchor *)b;
+
+    if (anchor_a->first_start < anchor_b->first_start) return -1;
+    if (anchor_a->first_start > anchor_b->first_start) return 1;
+    return 0;
+}
+
+int nob__cmp_by_anchor_idx(const void *a, const void *b) {
+    const Nob_Profile_Anchor *anchor_a = (const Nob_Profile_Anchor *)a;
+    const Nob_Profile_Anchor *anchor_b = (const Nob_Profile_Anchor *)b;
+
+    if (anchor_a->anchor_idx < anchor_b->anchor_idx) return -1;
+    if (anchor_a->anchor_idx > anchor_b->anchor_idx) return 1;
+    return 0;
+}
+
+void nob_log_profiler(Nob_Profiler profiler) {
+    assert(profiler.blocks.count == 0); // No open blocks should be present
+    u64 total_elapsed = nob_read_cpu_timer() - profiler.start;
+    printf("Total: %.2f ms\n", nob_measure_time_in_millis_from_elapsed(total_elapsed, profiler.cpu_freq));
+    qsort(profiler.anchors.items + 1, profiler.anchors.count - 1, sizeof(Nob_Profile_Anchor), nob__cmp_by_first_start);
+    for (size_t i = 1; i < profiler.anchors.count; i++) {
+        Nob_Profile_Anchor anchor = profiler.anchors.items[i];
+        if (anchor.label == NULL) continue;
+        f64 percent = 100 * (f64) anchor.total_elapsed_excluding_children / (f64) total_elapsed;
+        printf("  %s[%lu]: %.2f ms (%.2f%%", anchor.label, anchor.hit_count, nob_measure_time_in_millis_from_elapsed(anchor.total_elapsed_excluding_children, profiler.cpu_freq), percent);
+        if (anchor.total_elapsed_excluding_children != anchor.total_elapsed_including_children) {
+            f64 percent_with_children = 100 * (f64) anchor.total_elapsed_including_children / (f64) total_elapsed;
+            printf(", %.2f%% w/children", percent_with_children);
+        }
+        printf(")\n");
+    }
+    qsort(profiler.anchors.items + 1, profiler.anchors.count - 1, sizeof(Nob_Profile_Anchor), nob__cmp_by_first_start);
+}
+
+#endif // NOB_PROFILER_NO_STDLIB
+
 #endif // NOB_PROFILER_IMPLEMENTATION
 
 #ifndef NOB_PROFILER_STRIP_PREFIX_GUARD_
@@ -101,6 +234,18 @@ f64 nob_measure_time_in_millis_from_elapsed(u64 elapsed, f64 freq) {
         #define read_cpu_timer                      nob_read_cpu_timer
         #define guess_cpu_timer_freq                nob_guess_cpu_timer_freq
         #define measure_time_in_millis_from_elapsed nob_measure_time_in_millis_from_elapsed
+        #ifndef NOB_PROFILER_NO_STDLIB
+            #define Profile_Anchor           Nob_Profile_Anchor
+            #define Profile_Anchors          Nob_Profile_Anchors
+            #define Profile_Block            Nob_Profile_Block
+            #define Profile_Blocks           Nob_Profile_Blocks
+            #define Profiler                 Nob_Profiler
+            #define reset_profiler           nob_reset_profiler
+            #define start_profile_at_anchor  nob_start_profile_at_anchor
+            #define start_profile            nob_start_profile
+            #define end_profile              nob_end_profile
+            #define log_profiler             nob_log_profiler
+        #endif // NOB_PROFILER_NO_STDLIB
     #endif // NOB_UNSTRIP_PREFIX
 #endif // NOB_PROFILER_STRIP_PREFIX_GUARD_
 
--- test_nob_profiler.c
+++ test_nob_profiler.c
@@ -1,5 +1,97 @@
+#include <stdio.h>
+
+#include "nob.h"
 #define NOB_PROFILER_IMPLEMENTATION
+// #define NOB_PROFILER_NO_STDLIB
 #include "nob_profiler.h"
+
+Profiler profiler = {0};
+
+u64 fibonacci_recursive(u64 n) {
+    u64 result = 0;
+    start_profile(&profiler, "fibonacci_recursive");
+
+    if (n == 0) return_defer(0);
+    if (n <= 2) return_defer(1);
+    return_defer(fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2));
+
+defer:
+    end_profile(&profiler);
+    return result;
+}
+
+u64 fibonacci_iterative(u64 n) {
+    start_profile(&profiler, "fibonacci_iterative");
+    u64 f0 = 0;
+    u64 f1 = 1;
+    while (n > 0) {
+        u64 temp = f0 + f1;
+        f0 = f1;
+        f1 = temp;
+        n--;
+    }
+    end_profile(&profiler);
+    return f0;
+}
+
+u64 ackermann_recursive(u64 m, u64 n) {
+    u64 result = 0;
+    start_profile(&profiler, "ackermann_recursive");
+
+    if (m == 0) {
+        return_defer(n + 1);
+    } else if (m > 0 && n == 0) {
+        return_defer(ackermann_recursive(m - 1, 1));
+    } else {
+        return_defer(ackermann_recursive(m - 1, ackermann_recursive(m, n - 1)));
+    }
+
+defer:
+    end_profile(&profiler);
+    return result;
+}
+
+u64 tak(u64 x, u64 y, u64 z);
+
+u64 tak_x(u64 x, u64 y, u64 z) {
+    u64 result = 0;
+    start_profile(&profiler, "tak_branch_x");
+    return_defer(tak(x - 1, y, z));
+defer:
+    end_profile(&profiler);
+    return result;
+}
+
+u64 tak_y(u64 x, u64 y, u64 z) {
+    u64 result = 0;
+    start_profile(&profiler, "tak_branch_y");
+    return_defer(tak(y - 1, z, x));
+defer:
+    end_profile(&profiler);
+    return result;
+}
+
+u64 tak_z(u64 x, u64 y, u64 z) {
+    u64 result = 0;
+    start_profile(&profiler, "tak_branch_z");
+    return_defer(tak(z - 1, x, y));
+defer:
+    end_profile(&profiler);
+    return result;
+}
+
+u64 tak(u64 x, u64 y, u64 z) {
+    u64 result = 0;
+    start_profile(&profiler, "tak_main");
+    if (y < x) {
+        return_defer(tak(tak_x(x, y, z), tak_y(x, y, z), tak_z(x, y, z)));
+    } else {
+        return_defer(z);
+    }
+defer:
+    end_profile(&profiler);
+    return result;
+}
 
 int main(void) {
     f64 os_freq = get_os_timer_freq();
@@ -17,5 +109,39 @@ int main(void) {
     u64 elapsed_cpu = read_cpu_timer() - start_cpu;
     printf("OS Elapsed: %zu, Time Taken: %f millis\n", elapsed_os, measure_time_in_millis_from_elapsed(elapsed_os, os_freq));
     printf("CPU Elapsed: %zu, Time Taken: %f millis\n", elapsed_cpu, measure_time_in_millis_from_elapsed(elapsed_cpu, cpu_freq));
+
+    {
+        u64 n = 30;
+        reset_profiler(&profiler);
+        start_profile(&profiler, "fibonacci_recursive_start");
+        printf("fibonacci_recursive(%lu): %lu\n", n, fibonacci_recursive(n));
+        end_profile(&profiler);
+        log_profiler(profiler);
+
+        reset_profiler(&profiler);
+        start_profile(&profiler, "fibonacci_iterative_start");
+        printf("fibonacci_iterative(%lu): %lu\n", n, fibonacci_iterative(n));
+        end_profile(&profiler);
+        log_profiler(profiler);
+    }
+
+    {
+       u64 n = 4, k = 1;
+       reset_profiler(&profiler);
+       start_profile(&profiler, "ackermann_recursive_start");
+       printf("ackermann_recursive(%lu, %lu): %lu\n", n, k, ackermann_recursive(n, k));
+       end_profile(&profiler);
+       log_profiler(profiler);
+    }
+
+    {
+       u64 x = 24, y = 16, z = 8;
+       reset_profiler(&profiler);
+       start_profile(&profiler, "tak_start");
+       printf("tak(%lu, %lu, %lu): %lu\n", x, y, z, tak(x, y, z));
+       end_profile(&profiler);
+       log_profiler(profiler);
+    }
+
     return 0;
 }
