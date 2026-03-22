5c1021c2d798420946560d850fdecb1d82786f58
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2026-03-22 08:53:05 +0530

    Changed build command to use O2

--- nob.c
+++ nob.c
@@ -16,7 +16,7 @@ bool build_test_binary_exec(Test_Binary_Exec_Names *test_binary_exec_names, cons
     NOB_ASSERT((count >= 1) && "We need at least one source file to build the binary");
     int rebuild_is_needed = needs_rebuild(test_binary_exec, test_source_files, count);
     if (ALWAYS_BUILD || (rebuild_is_needed > 0)) {
-        cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", test_binary_exec, test_source_files[0]);
+        cmd_append(&cmd, "cc", "-O2", "-Wall", "-Wextra", "-o", test_binary_exec, test_source_files[0]);
         if (!cmd_run(&cmd, .async = &procs)) return NULL;
     }
     da_append(test_binary_exec_names, (char *) test_binary_exec);
